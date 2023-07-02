fbcb5af7487308795d6a3b914e2332252a618ddf
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-07-02 22:32:16 +0300

    Delete conn.php

--- conn.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$dbServername="localhost";
-$dbUsername="u728511153_users";
-$dbPassoword="|I3qooAtfz";
-$dbName="u728511153_users";
-
-$conn= mysqli_connect($dbServername, $dbUsername, $dbPassoword, $dbName);
