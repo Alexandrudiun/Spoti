61c04ccd45895448f914ddec29c4b92208d73dd9
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-07-02 22:49:23 +0300

    1

--- home.php
+++ home.php
@@ -22,6 +22,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="login.css">
+    <link rel="manifest" href="manifest.json">
     <title>Profil</title>
  </head>
 <style>
