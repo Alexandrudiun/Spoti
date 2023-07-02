15bd80435c68112452e270e62c99dfa775a48222
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-07-02 22:40:57 +0300

    Update home.php

--- home.php
+++ home.php
@@ -20,7 +20,7 @@ session_start();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">\
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="login.css">
     <title>Profil</title>
  </head>
