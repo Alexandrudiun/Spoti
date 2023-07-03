e083211c8f64c11f1e5de68594f8f026d13740fb
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-07-03 22:50:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ if(isset($_POST['submit'])){
             </div>
             <div style="display: flex; justify-content: center;">
                 <p style="font-size:12px; margin-top:15px;">U don't have an account? </p>
-                <p style="font-size:12px;"> <a href="register.php">Create an account now</a></p>
+                <p style="font-size:12px; margin-top:15px;"> <a href="register.php">Create an account now</a></p>
         </form>
     </div>
 </body>
