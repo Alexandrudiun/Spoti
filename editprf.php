6bb2fb3a1a8887d6edc3ef2da57e955240f329eb
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-07-14 21:03:39 +0300

    Update editprf.php

--- editprf.php
+++ editprf.php
@@ -37,6 +37,9 @@ else {
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Login</title>
     <link rel="stylesheet" type="text/css" href="/login.css">
     <link rel="manifest" href="manifest.json">
