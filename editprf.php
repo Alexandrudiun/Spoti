c3b6a285280c0139520afa71ef58129267cd5a49
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-07-14 18:44:18 +0300

    Update editprf.php

--- editprf.php
+++ editprf.php
@@ -80,12 +80,13 @@ else {
             <p>Name:</p>
             <input type="text" name="name" value="<?php echo $_SESSION['name'];?>">
             <br>
+            <div style="text-align: center; display:flex; justify-content:center;">
             <label class="buton" style="text-align: center;">
                 Click here to change your profile photo          
                 <input type="file" accept="image/*" name="image" id="image" onchange="displaySelectedPhoto(this)">
             </label>
             <br>
-            <div style="text-align: center; display:flex; justify-content:center;">
+            
                 
             <button class="buton" type="submit" name="submit" value="Update" class="buton">
                 Update profile photo
