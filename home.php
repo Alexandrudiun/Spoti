260453301adbcf4bd1da7c5f16445e196302a74b
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-07-15 21:03:33 +0300

    Update home.php

--- home.php
+++ home.php
@@ -86,7 +86,7 @@ session_start();
             <div class="buyBox">
             <div style="display: flex; gap: 25px; align-items: center;">
                 <h2>Buy</h2>
-                <div style="background-color: white; border-radius: 50px; border:1px solid black; width: 50px; height: 50px; display: flex; justify-content: center; align-items: center;">
+                <div style="background-color: #15f4f5; border-radius: 50px; border:1px solid black; width: 50px; height: 50px; display: flex; justify-content: center; align-items: center;">
                 <img src="assets/img/buy.svg" alt="buy" style="width: 47px; height: 47px;">
                 </div>
             </div>
