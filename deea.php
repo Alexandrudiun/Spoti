1b1c2f5bf0306fa741086b40479cd4073fccc5d8
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-07-22 12:58:56 +0300

    Create deea.php

---
+++ deea.php
@@ -0,0 +1,39 @@
+
+<?php
+
+if(isset($_POST['submit'])){
+
+    $m=$_POST['m'];
+    if($m=="ceccd19ee311aa73c14b7c8ff491ffed")
+{
+    header("Location: https://alexandrudiun.github.io/deeaandreea/");
+
+}
+else
+{
+    echo "Mai incearca o data";
+}
+
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><3</title>
+</head>
+<body>
+    <form action="" method="post" style="display:flex; text-align: center; justify-content: center;">
+     Ca sa deblochezi trebuie sa introduci parola <br>
+     Parola este insa criptata, asa ca trebuie sa o criptezi <br>
+     Foloseste codul ascuns pe ultima pagina a agendei si cripeaza l in algoritm MD5 <a href="https://www.dcode.fr/md5-hash">aici</a> <br>
+        <input type="text" name="m" id="m">
+        <input type="submit" value="submit" name="submit">
+    </form>
+
+</body>
+</html>
