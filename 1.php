b752d2407061fd711f77ff050a442096a157d2f3
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-07-24 16:16:02 +0300

    Update 1.php

--- 1.php
+++ 1.php
@@ -2,9 +2,9 @@
 include 'conn.php';
 $query = mysqli_query($conn, "SELECT * FROM users");
 while ($usr = mysqli_fetch_assoc($query)) {
+    $id = $usr['id'];
     $availability = $usr['availability'];
     $availability = $availability - 1;
-
     // Update the "availability" column
     $update_query = "UPDATE users SET availability = '$availability' WHERE id = '$id'";
     $update_user_query = mysqli_query($conn, $update_query);
