a05f4c3b2e37248e43df8521d05f66a1adb9c414
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-07-24 10:33:50 +0300

    Update 1.php

--- 1.php
+++ 1.php
@@ -1,32 +1,18 @@
 <?php
 include 'conn.php';
-session_start(); 
-    if(isset($_SESSION['email']) && isset($_SESSION['password'])){
-    $email = $_SESSION['email']; 
-    $email_search = "select * from users where email='$email'";
-    $query = mysqli_query($conn,$email_search);
-    $usr = mysqli_fetch_assoc($query);
+
+while ($usr = mysqli_fetch_assoc($query)) {
     $id = $usr['id'];
-    $name = $usr['name'];
-    $phone = $usr['phone'];
     $availability = $usr['availability'];
-    $date =$usr['date'];
-    $availability=$usr['$availability']+($date-date("Y-m-d"));
-    // echo $id;
-    // echo $name;
-    // echo $phone;
-    // echo $availability;
-    echo $date;
-    echo "<br>";
-    echo date("Y-m-d");
-    echo "<br>";
-  
-$presentDate = date('Y-m-d'); // Get the present date
-$daysBetween = floor((strtotime($presentDate) - strtotime($date)) / (60 * 60 * 24));
-$availability = $availability - $daysBetween;
+    $availability = $availability - 1;
 
+    // Update the "availability" column
+    $update_query = "UPDATE users SET availability = '$availability' WHERE id = '$id'";
+    $update_user_query = mysqli_query($conn, $update_query);
 
-} else {
-    header("Location: index.php");
+    // Check if the update was successful
+    if (!$update_user_query) {
+        die("Error updating record: " . mysqli_error($conn));
+    }
 }
 ?>
